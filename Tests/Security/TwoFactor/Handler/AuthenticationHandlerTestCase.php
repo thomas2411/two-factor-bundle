25317079cbdfa2d49f2fc6ef1f033078ad295d9e
Christian Scheb <privat@stylemotion.de>
2019-04-03 21:59:45 +0200

    Enable strict types everywhere

--- Tests/Security/TwoFactor/Handler/AuthenticationHandlerTestCase.php
+++ Tests/Security/TwoFactor/Handler/AuthenticationHandlerTestCase.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Scheb\TwoFactorBundle\Tests\Security\TwoFactor\Handler;
 
 use Scheb\TwoFactorBundle\Security\TwoFactor\AuthenticationContextInterface;
